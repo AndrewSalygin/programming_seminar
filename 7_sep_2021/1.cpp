12c8e483f6a9545b87d0faa259b3d1e173b0f8dd
AndrewSalygin <andrew.salygin@gmail.com>
2021-10-06 21:15:53 +0400

    Hip and Angle (English version)

--- 7_sep_2021/1.cpp
+++ 7_sep_2021/1.cpp
@@ -9,14 +9,14 @@ void main()
 {
     setlocale(LC_ALL, "Russian");
     double angle, hip;
-    cout << "¬ведите значение гипотенузы: ";
+    cout << "Enter the hypotenuse value: ";
     cin >> hip;
-    cout << "¬ведите значение угла: ";
+    cout << "Enter the angle value: ";
     cin >> angle;
 
     double x = hip * cos(angle * M_PI / 180.0); //One radian is equivalent to 180/PI degrees. 
     double y = hip * sin(angle * M_PI / 180.0); // 1 rad = 180/PI deg => n deg = PI/180 * n
 
-    cout << "ѕериметр: " << x + y + hip << '\n';
-    cout << "ѕлощадь: " << (x * y) / 2.0 << '\n';
+    cout << "Perimeter: " << x + y + hip << '\n';
+    cout << "Area: " << (x * y) / 2.0 << '\n';
 }
