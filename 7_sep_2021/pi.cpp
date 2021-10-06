e947fb79cef9a1ae9f9389c5f0f72aab4b52a03f
AndrewSalygin <andrew.salygin@gmail.com>
2021-10-06 23:35:13 +0400

    mend

--- 7_sep_2021/pi.cpp
+++ 7_sep_2021/pi.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main()
 {
     cout << acos(-1) << '\n';
+    cout << acos(0) * 2 << '\n';
     cout << asin(1) * 2 << '\n';
-    cout << atan(1) * 4;
+    cout << asin(-1) * (-2) << '\n';
+    cout << atan(1) * 4 << '\n';
+    cout << acos(1 / 2) * 2 << '\n';
+
     return 0;
 }
