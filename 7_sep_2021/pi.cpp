51ddd0d6e98f19aafec66acfcd5b0f41bb3a8eb8
AndrewSalygin <andrew.salygin@gmail.com>
2021-10-06 23:26:22 +0400

    PI

---
+++ 7_sep_2021/pi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    cout << acos(-1) << '\n';
+    cout << asin(1) * 2 << '\n';
+    cout << atan(0) * 2;
+    return 0;
+}
