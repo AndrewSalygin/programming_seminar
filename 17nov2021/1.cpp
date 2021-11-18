9a83bab7bac9f5c7324195c54f9c13d48076ea7f
Andrew Salygin <andrew.salygin@gmail.com>
2021-11-18 09:38:31 +0400

    Update setprecision

--- 17nov2021/1.cpp
+++ 17nov2021/1.cpp
@@ -9,14 +9,14 @@ int main()
     {
         sum += 1. / i;
     }
-    cout << fixed << setprecision(6) << sum << '\n';
+    cout << fixed << setprecision(20) << sum << '\n';
 
     sum = 0;
     for (int i = 100000; i > 0; i--)
     {
         sum += 1. / i;
     }
-    cout << fixed << setprecision(6) << sum << '\n';
+    cout << fixed << setprecision(20) << sum << '\n';
 
     return 0;
 }
