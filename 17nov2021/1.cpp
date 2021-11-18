5cb818d49ab5d21480ecd011f460575dd47c195a
Andrew Salygin <andrew.salygin@gmail.com>
2021-11-18 09:43:32 +0400

    add n

--- 17nov2021/1.cpp
+++ 17nov2021/1.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main()
 {
+    int n = 100000;
     double sum = 0;
-    for (int i = 1; i <= 100000; i++)
+    for (int i = 1; i <= n; i++)
     {
         sum += 1. / i;
     }
     cout << fixed << setprecision(20) << sum << '\n';
 
     sum = 0;
-    for (int i = 100000; i > 0; i--)
+    for (int i = n; i > 0; i--)
     {
         sum += 1. / i;
     }
